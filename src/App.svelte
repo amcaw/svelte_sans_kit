42c6adb5ed845cc48c468c69eb65ad8240322d42
amcaw <42608053+amcaw@users.noreply.github.com>
2023-09-04 13:54:44 +0200

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+import Fa from 'svelte-fa'
+import { faBolt, faFire } from '@fortawesome/free-solid-svg-icons'
+
+import { csv } from 'd3-fetch';
+
+import { onMount } from 'svelte';
+
+let datapoints = [];
+	onMount(() => {
+		csv("https://raw.githubusercontent.com/amcaw/energizor/main/BE_day.csv")
+			.then((res) => {
+			datapoints = res;
+		})
+	})
+
+  
+  $: ajd = datapoints.slice(-2, -1)
+  
+  
+  </script>
+
+{#each datapoints as datapoints}
+<p>{datapoints.Value}</p>
+
+{/each}
+
+
